72ae2f56b0cf8175a617f1a766aaa78bbf628fb3
Abdallah Bakr <abab231196@gmail.com>
2024-12-16 22:49:04 +0200

    add mongo db

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,
+from helpers.config import get_settings
 from routes import base_router, data_router
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+    settings = get_settings()
+    app.mongo_connection = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_connection[settings.MONGODB_DATABASE]
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_connection.close()
 
 app.include_router(base_router)
 app.include_router(data_router)
