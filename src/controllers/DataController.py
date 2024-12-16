ef8f1caed07f37db011266a5090cd6934dbf6e2e
Abdallah Bakr <abab231196@gmail.com>
2024-12-16 12:54:29 +0200

    update routing and controller

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,9 @@
-from fastapi import UploadFile
 from .BaseController import BaseController
+from .ProjectController import ProjectController
+from fastapi import UploadFile
 from models import ResponseSignal
+import re
+import os
 
 
 class DataController(BaseController):
@@ -17,4 +20,25 @@ class DataController(BaseController):
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
             return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
         
-        return True, "success"
+        return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
+    
+    def generate_unique_filename(self, orig_file_name: str, project_id:str):
+
+        project_path = ProjectController().get_project_path(project_id)
+        cleaned_file_name = self.get_clean_file_name(orig_file_name)
+
+        file_name = ""
+        new_file_path = ""
+        while True:
+            random_key = self.generate_random_string()
+            file_name = random_key +"_"+ cleaned_file_name
+            new_file_path = os.path.join(project_path, file_name)
+
+            if not os.path.exists(new_file_path):
+                break
+        
+        return new_file_path, file_name
+
+    def get_clean_file_name(self, orig_file_name:str):
+        # remove any special characters, except underscore and.
+        return re.sub(r'[^\w.]', '', orig_file_name.strip()).replace(" ", "_")
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, Depends, UploadFile, status
+from controllers import DataController, ProjectController
 from helpers.config import get_settings, Settings
-from controllers import DataController
+from fastapi.responses import JSONResponse
+from models import ResponseSignal
+import logging
+import aiofiles
+
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -12,7 +18,37 @@ async def upload_data(project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
 
     # validate the file properties
-    is_valid, result_signal = DataController().validate_uploaded_file(file=file)
-    return {
-        "signal": result_signal
-    }
+    data_controller = DataController()
+    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
+    
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": result_signal
+            }
+        )
+    
+    file_path, file_id = data_controller.generate_unique_filename(file.filename, project_id)
+
+    try:
+        async with aiofiles.open(file_path, 'wb') as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as ex:
+        logging.error(f"Error while uploading file: {ex}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED
+            }
+        )
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            "file_id": file_id
+        }
+    )
+    
